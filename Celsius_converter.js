8ff49cce796ec3881dfd738549b4060844df5b3d
angularOcean <liher@oregonstate.edu>
2021-12-20 21:00:21 -0800

    added new file

---
+++ Celsius_converter.js
@@ -0,0 +1,8 @@
+//Celsius to Fahrenheit Converter
+//This codes a simple function to convert celsius to fahrenheit
+
+function convertToF(celsius) {
+    let Fahrenheit = celsius * 9/5 + 32
+    return Fahrenheit
+}
+
